bbe819a2ea2ddb12c1575ebb075008e19da402b2
Eddie Hung <eddieh@ece.ubc.ca>
2019-02-08 07:01:33 -0800

    Generate ASC, and pipe it through icebox_vlog for sanity

--- ice40/regressions/issue0084/mlaccel.sh
+++ ice40/regressions/issue0084/mlaccel.sh
@@ -1,3 +1,3 @@
 :> mlaccel.log
-${NPNR} --json mlaccel.json --pcf mlaccel.pcf `cat mlaccel.npnr` > mlaccel.log 2>&1 || true
+${NPNR} --json mlaccel.json --asc mlaccel.asc --pcf mlaccel.pcf `cat mlaccel.npnr` > mlaccel.log 2>&1 || true
 grep "^ERROR: PACKAGE_PIN of SB_IO 'qpi_in_buf\\[1\\]' connected to more than a single top level IO.$" mlaccel.log
--- ice40/regressions/issue0209/test.sh
+++ ice40/regressions/issue0209/test.sh
@@ -1,3 +1,3 @@
 :> test.log
-${NPNR} --json test.json --pcf test.pcf `cat test.npnr` > test.log 2>&1 || true
+${NPNR} --json test.json --asc test.asc --pcf test.pcf `cat test.npnr` > test.log 2>&1 || true
 grep "^ERROR: BEL 'X0/Y28/io0' has no global buffer connection available$" test.log
