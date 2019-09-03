8f93e7e0f897b1b5da469919c9a43ba28b623b2a
David Shah <dave@ds0.me>
2019-09-03 11:54:19 +0100

    ice40: Fix up5k wire count
    Signed-off-by: David Shah <dave@ds0.me>

--- ice40/up5k.cc
+++ ice40/up5k.cc
@@ -59,7 +59,7 @@ TEST_F(UP5KTest, wire_names)
         assert(wire == ctx->getWireByName(name));
         wire_count++;
     }
-    ASSERT_EQ(wire_count, 124503);
+    ASSERT_EQ(wire_count, 124523);
 }
 
 TEST_F(UP5KTest, pip_names)
