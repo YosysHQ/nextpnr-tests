00c55a9eb9ea2e062b51fe0d64741412b185d95d
gatecat <gatecat@ds0.me>
2022-04-08 16:34:55 +0100

    Fix for latest Yosys
    Signed-off-by: gatecat <gatecat@ds0.me>

--- generic/flow/bel-pin/run.sh
+++ generic/flow/bel-pin/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
-yosys -q -p "prep; write_json design.json" design.v
+yosys -q -p "prep -top top; write_json design.json" design.v
 $NPNR --json design.json --pre-pack pre_pack.py --post-route post_route.py --no-iobs
